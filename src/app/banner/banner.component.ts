7557853dbbe34ad9c53a98c86dbf287761541cf9
Márcio Marques <mar.info.rj@gmail.com>
2026-01-31 15:46:19 +0100

    Usando input

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { BoasVindasComponent } from "./boas-vindas/boas-vindas.component";
 import { ContaComponent } from "./conta/conta.component";
 
@@ -10,5 +10,6 @@ import { ContaComponent } from "./conta/conta.component";
 })
 export class BannerComponent {
 
+  saldo = input.required<number>();
 
 }
