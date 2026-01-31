06b97708f2ede519f459d4e535f0cbf8613fec71
Márcio Marques <mar.info.rj@gmail.com>
2026-01-31 11:38:38 +0100

    nuovi componenti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
+import { Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, FormNovaTransacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'anybank';
+  processarTransacao(transacao : Transacao){
+    console.log(transacao);
+  }
 }
---
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -0,0 +1,30 @@
+import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TipoTransacao, Transacao } from '../modelos/transacao';
+
+@Component({
+  selector: 'app-form-nova-transacao',
+  imports: [FormsModule],
+  templateUrl: './form-nova-transacao.component.html',
+  styleUrl: './form-nova-transacao.component.css'
+})
+export class FormNovaTransacaoComponent {
+
+  tipoTransacao = "";
+  valorTransacao = "";
+
+  transacaoCriada = output<Transacao>();
+
+  aoSubmeter(){
+
+    const transacao = new Transacao(
+      this.tipoTransacao as TipoTransacao, 
+      Number(this.valorTransacao))
+
+    this.transacaoCriada.emit(transacao);
+
+    this.tipoTransacao = "";
+    this.valorTransacao = "";
+  }
+
+}
