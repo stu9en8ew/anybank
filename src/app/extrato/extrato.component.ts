40f39513e1c66becbd3f759a7be4130b6f2d4059
Márcio Marques <mar.info.rj@gmail.com>
2026-02-01 09:20:36 +0100

    Gestione delle transazioni

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { TransacaoComponent } from "./transacao/transacao.component";
+import { Transacao } from '../modelos/transacao';
 
 @Component({
   selector: 'app-extrato',
@@ -8,5 +9,7 @@ import { TransacaoComponent } from "./transacao/transacao.component";
   styleUrl: './extrato.component.css'
 })
 export class ExtratoComponent {
+
+  transacoes = input.required<Transacao[]>();
 
 }
--- src/app/extrato/transacao/transacao.component.ts
+++ src/app/extrato/transacao/transacao.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Transacao } from '../../modelos/transacao';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-transacao',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './transacao.component.html',
   styleUrl: './transacao.component.css'
 })
 export class TransacaoComponent {
+
+  transacao = input.required<Transacao>()
 
 }
