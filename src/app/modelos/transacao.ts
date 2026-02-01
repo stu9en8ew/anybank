a7876a8863257191c9eddf1d23258fdb6341ecf3
Márcio Marques <mar.info.rj@gmail.com>
2026-02-01 09:29:47 +0100

    Usando libreria nanoId

--- src/app/modelos/transacao.ts
+++ src/app/modelos/transacao.ts
@@ -1,4 +1,8 @@
+import { nanoid } from "nanoid";
+
 export class Transacao {
+    readonly id = nanoid();
+    
     constructor(
        public readonly tipo: TipoTransacao,
        public readonly valor: number
